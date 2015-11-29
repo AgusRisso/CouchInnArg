a31967cca1bc491add59d008edf7546e7f592b02
Agustin L. Risso <agustinlrisso@hotmail.com.ar>
2015-11-28 23:35:32 -0300

    config-environments-production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'fierce-reaches-3694.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'fierce-reaches-3694.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "couchinnargentina@gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "couchinnargentina@gmail.com",
+    password: "ingenieria.software"
+  }
 end
